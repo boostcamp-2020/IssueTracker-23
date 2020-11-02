99a1d67965a7667ec3e13031044086fc8ab36099
Eunchae Jeon <grayfreesia@gmail.com>
2020-11-02 17:02:34 +0900

    feat: comment table generate
    models/comment.js 와 migration.js 생성

---
+++ server/db/models/comment.js
@@ -0,0 +1,40 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Comment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      models.comment.belongsTo(models.user, { foreignKey: 'author' });
+      models.comment.belongsTo(models.issue, { foreignKey: 'issue_id' });
+    }
+  }
+  Comment.init(
+    {
+      description: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      author: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      issueId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'comment',
+      underscored: true,
+      freezeTableName: true,
+      paranoid: true,
+    }
+  );
+  return Comment;
+};
