c9a6899178340d1886322fa290e59b33133d254b
Eunchae Jeon <grayfreesia@gmail.com>
2020-11-03 01:20:26 +0900

    fix: deleteAt -> deletedAt 오타 수정
    create-comment migration파일 오타 수정

--- server/db/migrations/20201102055312-create-comment.js
+++ server/db/migrations/20201102055312-create-comment.js
@@ -11,7 +11,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.TEXT,
       },
-      deleteAt: {
+      deletedAt: {
         type: Sequelize.DATE,
         field: 'deleted_at',
       },
