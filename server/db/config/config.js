40b04017aa4addc66d6878b48a176469fdaf02e2
Eunchae Jeon <grayfreesia@gmail.com>
2020-11-03 10:55:56 +0900

    refactor: db/config 파일 server/config에서 관리하도록 수정합니다
    db/config/config.js 는 server/config/index.js를 가져오는 역할만 하도록 수정

--- server/db/config/config.js
+++ server/db/config/config.js
@@ -1,30 +1,3 @@
-require('dotenv').config({ path: '../../.env' });
+const config = require('../../config');
 
-const { env } = process;
-
-const development = {
-  username: env.DB_USERNAME,
-  password: env.DB_PASSWORD,
-  database: env.DB_DEV_NAME,
-  host: env.HOST,
-  port: parseInt(env.DB_PORT, 10),
-  dialect: env.DB_DIALECT,
-};
-const test = {
-  username: env.DB_USERNAME,
-  password: env.DB_PASSWORD,
-  database: env.DB_DEV_NAME,
-  host: env.HOST,
-  port: parseInt(env.DB_PORT, 10),
-  dialect: env.DB_DIALECT,
-};
-const production = {
-  username: env.DB_USERNAME,
-  password: env.DB_PASSWORD,
-  database: env.DB_PROD_NAME,
-  host: env.HOST,
-  port: parseInt(env.DB_PORT, 10),
-  dialect: env.DB_DIALECT,
-};
-
-module.exports = { development, test, production };
+module.exports = config;
