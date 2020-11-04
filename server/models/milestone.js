9a7bc47f54089a6f5467625aa032050aa5c9bf7a
Kang Dong Hoon <ypd01018@naver.com>
2020-11-04 16:05:02 +0900

    feat : milestone 함수 변경 및 추가
    - read를 readAll로 변경 및 nClose, nOpen 반환 추가
    - open,close를 지원하는 updateOpenState 추가

--- server/models/milestone.js
+++ server/models/milestone.js
@@ -1,22 +1,29 @@
 const db = require('../db/models').milestone;
+const model = require('../db/models');
+const getDateTime = require('../util/date');
 
 class MilestoneModel {
   static create(milestoneData) {
     return db.create(milestoneData);
   }
 
-  static read(repositoryId) {
-    return db.findAll({
-      where: {
-        repositoryId,
-      },
+  static async readAll(repositoryId) {
+    const query = `SELECT A.id,A.title,A.description,A.due_date,A.closed_at,A.created_at,A.updated_at,COUNT(if(B.closed_at IS NULL,1,null)) AS nOpen,COUNT(if(B.closed_at IS NOT NULL,B.closed_at,null)) AS nClose
+    FROM milestone A 
+    LEFT join issue B
+    ON A.id = B.milestone_id
+    WHERE B.repository_id = :repository_id
+    GROUP by A.id`;
+
+    return model.sequelize.query(query, {
+      replacements: { repository_id: repositoryId },
     });
   }
 
-  static update(milestoneData) {
+  static update(milestoneData, milestoneId) {
     return db.update(milestoneData, {
       where: {
-        id: milestoneData.id,
+        id: milestoneId,
       },
     });
   }
@@ -27,6 +34,18 @@ class MilestoneModel {
         id: milestoneId,
       },
     });
+  }
+
+  static updateOpenState(milestoneId, isOpen) {
+    const closeAt = isOpen ? null : getDateTime();
+    return db.update(
+      { closeAt },
+      {
+        where: {
+          milestoneId,
+        },
+      }
+    );
   }
 }
 
