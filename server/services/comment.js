3e8c49355b21f57f0a2c1cadbee0ccdc0a504381
Eunchae Jeon <grayfreesia@gmail.com>
2020-11-04 18:10:48 +0900

    refacotr: comment service 반환값 변경
    객체로 반환하도록 변경

--- server/services/comment.js
+++ server/services/comment.js
@@ -1,20 +1,23 @@
 const CommentModel = require('../models/comment');
 
 class CommentSevice {
-  static create({ description, author, issueId }) {
-    return CommentModel.create({
+  static async create({ description, author, issueId }) {
+    const result = await CommentModel.create({
       description,
       author,
       issueId,
     });
+    return { id: result.dataValues.id };
   }
 
-  static update({ commentId, description }) {
-    return CommentModel.update({ commentId, description });
+  static async update({ commentId, description }) {
+    const [result] = await CommentModel.update({ commentId, description });
+    return result === 0 ? null : { id: commentId };
   }
 
-  static delete({ commentId }) {
-    return CommentModel.delete({ commentId });
+  static async delete({ commentId }) {
+    const result = await CommentModel.delete({ commentId });
+    return result === 0 ? null : { id: commentId };
   }
 }
 
