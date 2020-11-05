5064bbb8ebc5ebf256fed9277a55c3492a982392
Eunchae Jeon <grayfreesia@gmail.com>
2020-11-05 11:46:29 +0900

    feat: index router에 comments 추가
    /api/comments 경로에 commentRouter 등록

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 const labelRouter = require('./api/label');
+const commentRouter = require('./api/comment');
 
 router.use('/labels', labelRouter);
+router.use('/comments', commentRouter);
 
 module.exports = router;
