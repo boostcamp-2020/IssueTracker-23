ba369c61ebe346b66c835d9f2bce7e7a58dbe61e
JinKwan Kim <kkole3897@naver.com>
2020-11-05 11:30:41 +0900

    feat: milestoneRouter 모듈 사용 추가
    milestoneRouter 모듈 사용할 수 있도록 url에 추가

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 const labelRouter = require('./api/label');
+const milestoneRouter = require('./api/milestone');
 
 router.use('/labels', labelRouter);
-
+router.use('/milestones', milestoneRouter);
 module.exports = router;
