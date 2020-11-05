e2fa14fb1f00a96edec975a187485c14ba45f122
Eunchae Jeon <grayfreesia@gmail.com>
2020-11-05 21:53:40 +0900

    feat: index router에 issue router 등록
    /api/issues 에 issueRouter 등록

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const labelRouter = require('./api/label');
 const milestoneRouter = require('./api/milestone');
 const commentRouter = require('./api/comment');
+const issueRouter = require('./api/issue');
 
 const router = express.Router();
 
 router.use('/labels', labelRouter);
 router.use('/milestones', milestoneRouter);
 router.use('/comments', commentRouter);
+router.use('/issues', issueRouter);
 
 module.exports = router;
