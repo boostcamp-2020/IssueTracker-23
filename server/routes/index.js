414ad733717e397b00dfd283008e5507007a2cb4
Kang Dong Hoon <ypd01018@naver.com>
2020-11-06 00:12:05 +0900

    feat : 이미지 업로드 라우터 추가
    이미지 업로드 시 성공하면 url 반환
    실패 시 400 응답코드 반환

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const labelRouter = require('./api/label');
 const milestoneRouter = require('./api/milestone');
 const commentRouter = require('./api/comment');
+const imageRouter = require('./api/image');
 
 const router = express.Router();
 
 router.use('/labels', labelRouter);
 router.use('/milestones', milestoneRouter);
 router.use('/comments', commentRouter);
-
+router.use('/image', imageRouter);
 module.exports = router;
