2b448b7d641e893bdfed7b4ad3a906db529c1757
ypd01018 <37091190+ypd01018@users.noreply.github.com>
2020-11-06 00:41:07 +0900

    fix : milestone model에서 레포지토리를 고정값으로 받던 것을 수정

--- server/models/milestone.js
+++ server/models/milestone.js
@@ -13,7 +13,7 @@ class MilestoneModel {
     FROM milestone A 
     LEFT join issue B
     ON A.id = B.milestone_id
-    WHERE A.repository_id = 1 AND A.deleted_at IS NULL AND B.deleted_at IS NULL
+    WHERE A.repository_id = :repository_id AND A.deleted_at IS NULL AND B.deleted_at IS NULL
     GROUP by A.id`;
 
     return model.sequelize.query(query, {
