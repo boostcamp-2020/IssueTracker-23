42a6a62c4a0e3a7de270ca1b041cbb94b0e65548
JinKwan Kim <kkole3897@naver.com>
2020-11-10 15:13:31 +0900

    feat: Label, Milestone Button 추가
    Label 목록, Milestone 목록 화면으로 넘어갈 수 있도록 추가

--- client/src/pages/IssueList/IssueListNav.jsx
+++ client/src/pages/IssueList/IssueListNav.jsx
@@ -23,15 +23,53 @@ const FlexContainer = styled.div`
   display: flex;
 `;
 
+const FlexContainerSpaceBetween = styled(FlexContainer)`
+  justify-content: space-between;
+`;
+
+const LabelLink = styled(DefaultButton)`
+  border-top-right-radius: 0;
+  border-bottom-right-radius: 0;
+`;
+
+const MilestoneLink = styled(DefaultButton)`
+  margin-left: -1px;
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+`;
+
+const Counter = styled.span`
+  min-width: 20px;
+  padding: 0 6px;
+  border: 1px solid transparent;
+  border-radius: 2em;
+  background-color: #d1d5da80;
+  font-size: 12px;
+  font-weight: 500;
+  text-align: center;
+`;
+
 const IssueListNav = () => {
   return (
-    <FlexContainer>
-      <FilterButton>
-        Filters
-        <DropdownCaret />
-      </FilterButton>
-      <SearchBar />
-    </FlexContainer>
+    <FlexContainerSpaceBetween>
+      <FlexContainer>
+        <FilterButton>
+          Filters
+          <DropdownCaret />
+        </FilterButton>
+        <SearchBar />
+      </FlexContainer>
+      <FlexContainer>
+        <LabelLink buttonColor={'white'}>
+          Labels
+          <Counter>0</Counter>
+        </LabelLink>
+        <MilestoneLink buttonColor={'white'}>
+          Milestones
+          <Counter>0</Counter>
+        </MilestoneLink>
+      </FlexContainer>
+    </FlexContainerSpaceBetween>
   );
 };
 
