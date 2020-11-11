0b9bb49a1ffd12daf66105ce36953b50893ff631
ByeongjaeJeon <46068738+ByeongjaeJeon@users.noreply.github.com>
2020-11-12 02:29:25 +0900

    feat: IssueDetailItem에 프로필 사진 추가
    프로필 동그란 사진 옆에 IssueDeatil header,body 오도록 사진 추가

--- client/src/pages/IssueDetail/IssueDetailItem.jsx
+++ client/src/pages/IssueDetail/IssueDetailItem.jsx
@@ -2,13 +2,23 @@
 import React from 'react';
 import DescriptionHeader from '@Components/DescriptionHeader.jsx';
 import DescriptionBody from '@Components/DescriptionBody.jsx';
+import UserProfileUrl from '@Components/UserProfileUrl.jsx';
+import styled from 'styled-components';
+
+const IssueDetailItemStyle = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 
 const IssueDetailItem = (props) => {
   return (
-    <div>
-      <DescriptionHeader isOwner={props.isOwner} value={props.value} />
-      <DescriptionBody value={props.value} />
-    </div>
+    <IssueDetailItemStyle>
+      <UserProfileUrl author={props.value.author} />
+      <div style={{ width: '100%' }}>
+        <DescriptionHeader isOwner={props.isOwner} value={props.value} />
+        <DescriptionBody value={props.value} />
+      </div>
+    </IssueDetailItemStyle>
   );
 };
 
