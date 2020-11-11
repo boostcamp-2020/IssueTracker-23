efbe224e0ea3b364dfaaa875ba0bb1d64416ded2
Eunchae Jeon <grayfreesia@gmail.com>
2020-11-12 02:26:18 +0900

    feat: EditorWrapper Title추가, 말풍선 모양으로 스타일 적용
    Title component를 EditorWrapper 아래에 추가
    BubbleTail로 말풍선 꼬리 표현
    UserProfile 임시 프로필 표현

--- client/src/components/EditorWrapper.jsx
+++ client/src/components/EditorWrapper.jsx
@@ -3,23 +3,60 @@ import styled from 'styled-components';
 import DescriptionEditor from './DescriptionEditor.jsx';
 import EditorNavbar from './EditorNavbar.jsx';
 import EditorButtonWrapper from './EditorButtonWrapper.jsx';
+import TitleEditor from './TitleEditor.jsx';
 
 const StyledEditorWrapper = styled.div`
+  // margin: 100px;
   border: 1px solid #ccc;
   border-radius: 5px;
   width: max-content;
 `;
 
+const BubbleTail = styled.div`
+  position: relative;
+  top: 25px;
+  left: 5px;
+  width: 10px;
+  height: 10px;
+  transform: rotate(45deg);
+  border: 1px solid #ccc;
+  border-top: none;
+  border-right: none;
+  background-color: white;
+`;
+
+const BubbleWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+`;
+
+const UserProfile = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 5px;
+  overflow: hidden;
+  object-fit: cover;
+`;
+
 const EditorWrapper = ({ width, height, padding }) => {
   return (
-    <StyledEditorWrapper width={width}>
-      <EditorNavbar />
-      <DescriptionEditor
-        width={width - 2 * padding}
-        height={height - 2 * padding}
+    <BubbleWrapper>
+      <UserProfile
+        src={
+          'https://github.githubassets.com/images/modules/logos_page/Octocat.png'
+        }
       />
-      <EditorButtonWrapper />
-    </StyledEditorWrapper>
+      <BubbleTail />
+      <StyledEditorWrapper width={width}>
+        <TitleEditor />
+        <EditorNavbar />
+        <DescriptionEditor
+          width={width - 2 * padding}
+          height={height - 2 * padding}
+        />
+        <EditorButtonWrapper />
+      </StyledEditorWrapper>
+    </BubbleWrapper>
   );
 };
 
