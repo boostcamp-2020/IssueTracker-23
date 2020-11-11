660be18f2b1df5f5a889907461ade11bafe80d30
JinKwan Kim <kkole3897@naver.com>
2020-11-12 01:13:03 +0900

    refactor: 버튼들과 검색창 분리
    각각 컴포넌트 단위로 다른 파일로 분리하고 재사용 가능하게 만듦

--- client/src/components/IssueListNav.jsx
+++ client/src/components/IssueListNav.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import DefaultButton from './DefaultButton';
-import FilterDropdown from './FilterDropdown.jsx';
-
-const SearchBar = styled.input`
-  width: 100%;
-  margin-left: -1px;
-  padding: 5px 12px;
-  border: 1px solid #d1d5da;
-  border-radius: 0 6px 6px 0;
-  background-color: #fafbfc;
-  color: #586069;
-  font-size: 14px;
-`;
+import SearchBar from './SearchBar';
+import LabelButton from './LabelButton';
+import MilestoneButton from './MilestoneButton';
+import NewIssueButton from './NewIssueButton';
 
 const FlexContainer = styled.div`
   display: flex;
@@ -27,55 +18,19 @@ const FlexContainerJustifyBetween = styled(FlexContainer)`
   justify-content: space-between;
 `;
 
-const LabelLink = styled(DefaultButton)`
-  border-top-right-radius: 0;
-  border-bottom-right-radius: 0;
-`;
-
-const MilestoneLink = styled(DefaultButton)`
-  margin-left: -1px;
-  border-top-left-radius: 0;
-  border-bottom-left-radius: 0;
-`;
-
-const Counter = styled.span`
-  min-width: 20px;
-  padding: 0 6px;
-  border: 1px solid transparent;
-  border-radius: 2em;
-  background-color: #d1d5da80;
-  font-size: 12px;
-  font-weight: 500;
-  text-align: center;
-`;
-
-const newIssueButtonProps = {
-  backgroundColor: '#2ea44f',
-  borderColor: '#2A8645',
-  textColor: 'white',
-};
-
-const NewIssueButton = DefaultButton;
-
-const IssueListNav = () => {
+const IssueListNav = (props) => {
+  const { labels, milestones } = props;
   return (
     <FlexContainerJustifyBetween>
       <FlexContainerWidthFull>
-        <FilterDropdown />
-        <SearchBar />
+        <SearchBar placeholder="Search all issues" />
         <FlexContainer margin={'0 0 0 16px'}>
-          <LabelLink buttonColor={'white'}>
-            Labels
-            <Counter>0</Counter>
-          </LabelLink>
-          <MilestoneLink buttonColor={'white'}>
-            Milestones
-            <Counter>0</Counter>
-          </MilestoneLink>
+          <LabelButton showCounter={true} labels={labels} />
+          <MilestoneButton showCounter={true} milestones={milestones} />
         </FlexContainer>
       </FlexContainerWidthFull>
       <FlexContainerJustifyBetween margin={'0 0 0 16px'}>
-        <NewIssueButton {...newIssueButtonProps}>New Issue</NewIssueButton>
+        <NewIssueButton />
       </FlexContainerJustifyBetween>
     </FlexContainerJustifyBetween>
   );
