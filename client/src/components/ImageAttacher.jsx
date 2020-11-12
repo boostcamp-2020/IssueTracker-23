1131db6325b17c0d0b34a0d7cca8bd5425de3deb
Eunchae Jeon <grayfreesia@gmail.com>
2020-11-12 15:19:10 +0900

    feat: ImageAttacher image upload 기능 추가
    버튼 눌러서 파일 업로드 하면 마크다운 형식으로 image 정보 표시

--- client/src/components/ImageAttacher.jsx
+++ client/src/components/ImageAttacher.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
 const StyledImageAttacher = styled.button`
   border: none;
@@ -12,10 +13,52 @@ const StyledImageAttacher = styled.button`
   color: #555;
 `;
 
-const ImageAttacher = () => (
-  <StyledImageAttacher>Attach files by selecting here.</StyledImageAttacher>
-);
+const ImageAttacher = ({ host }) => {
+  const fileInput = useRef();
 
-ImageAttacher.defaultProps = {};
+  const onClick = useCallback((e) => {
+    if (e.target !== e.currentTarget) return;
+    fileInput.current.click();
+  }, []);
+
+  const onChange = useCallback(
+    (e) => {
+      const formData = new FormData();
+      const image = e.target.files[0];
+      formData.append('image', image);
+      const textareaElement =
+        fileInput.current.parentNode.parentNode.firstChild;
+      const newLine = textareaElement.value.slice(-1) === '\n' ? '' : '\n';
+      textareaElement.value += `${newLine}![Uploading ${image.name}...]()`;
+      return axios
+        .post(`${host}/api/image`, formData)
+        .then((res) => {
+          textareaElement.value = textareaElement.value.replace(
+            `![Uploading ${image.name}...]()`,
+            `![${image.name}](${res.data.imageUrl})\n`
+          );
+        })
+        .catch((err) => {});
+    },
+    [host]
+  );
+
+  return (
+    <StyledImageAttacher onClick={onClick} host={host}>
+      <input
+        type="file"
+        name="image"
+        hidden
+        onChange={onChange}
+        ref={fileInput}
+      />
+      Attach files by selecting here.
+    </StyledImageAttacher>
+  );
+};
+
+ImageAttacher.defaultProps = {
+  host: 'http://localhost:3000',
+};
 
 export default ImageAttacher;
